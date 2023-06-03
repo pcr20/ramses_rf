33337fe23f58c407b01f7353c9100b2989414a51
Paul Roberts <dr.paul.roberts@gmail.com>
2023-06-03 20:54:33 +0100

    removed constructor (presumably it will case the parent constructor)

--- ramses_rf/protocol_esphomeapi.py
+++ ramses_rf/protocol_esphomeapi.py
@@ -2,18 +2,21 @@ import logging
 from serial import SerialBase
 
 _LOGGER=logging.getLogger(__name__)
-class klass(SerialBase):
-    def __init__(self, junk, *args, **kwargs):
-        _LOGGER.debug("klass __init__ called: %s",self)
-        return
+class Serial(SerialBase):
+#    def __init__(self, junk, *args, **kwargs):
+#        _LOGGER.debug("klass __init__ called: %s",self)
+#        return
     def open(self):
         _LOGGER.debug("klass open called: %s",self)
         return
 
 
 
+
 def serial_class_for_url(url):
 
-    _LOGGER.debug("serial_class_for_url called: %s %s",url,klass)
+    _LOGGER.debug("serial_class_for_url called: %s %s",url,Serial)
 
-    return url,klass
+    return url,Serial
+    
+
